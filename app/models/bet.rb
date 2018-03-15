f52d0050496a103ed51ab5217ea11cbaf912f08a
Adam Diel <adamdiel@gmail.com>
2018-03-14 21:25:45 -0700

    Improved the relations and ability to call sub bets.
    Can now call @user.bets properly.
    
    Improved the display page of the user

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,4 +1,4 @@
 class Bet < ApplicationRecord
-  belongs_to :owner1, :class_name => "User"
-  belongs_to :owner2, :class_name => "User"
+  belongs_to :owner1, :class_name => "User", :foreign_key => 'owner1_id'
+  belongs_to :owner2, :class_name => "User", :foreign_key => 'owner2_id'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-  has_many :bets
+  has_many :owner_2_bets, class_name: 'Bet', foreign_key: "owner2_id"
+  has_many :owner_1_bets, class_name: 'Bet', foreign_key: "owner1_id"
+  def bets
+    owner_1_bets + owner_2_bets
+  end
 end
